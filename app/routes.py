dd9a894ca8cdcd83a1db28902a0d25ca6437d05e
rajat6885 <52233378+rajat6885@users.noreply.github.com>
2019-06-27 09:49:00 +0530

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+@app.route('/')
+@app.route('/index')
+def index():
+    user={"username":'shekhar'}
+    return'''
+    <html>
+    <head>
+<title>My Application</title>
+    </head>
+    <body bgcolor='cyan'>
+<h1>Hello,'''+ user['username']+'''!</h1>
+    </body>
+    </html>'''
